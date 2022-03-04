c89160467e28f61095391db6408650cbbae112ae
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2022-03-04 14:46:29 +0900

    4

---
+++ 모두의 알고리즘/4/2.py
@@ -0,0 +1,13 @@
+def findMax(idx, max):
+
+    a = [2, 1, 4, 7, 0]
+    lenth = len(a)
+    if(idx <= 0):
+        return max
+    if a[idx-1] > max:
+        max = a[idx]
+
+    return findMax(idx-1, max)
+
+
+print(findMax(5, 0))
