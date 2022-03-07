43b588ef67da2cfffbb49ce8cd9e92999e84828e
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2022-03-08 00:12:45 +0900

    11

---
+++ 모두의 알고리즘/11/1.py
@@ -0,0 +1,11 @@
+def findAlpa(x):
+    length = len(x)
+
+    for i in range(length):
+        if i < (length-1)-i:
+            if x[i].lower() != x[(length-1)-i].lower():
+                return False
+    return True
+
+
+print(findAlpa("Hi"))
